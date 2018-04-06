727ab54282c673a6d170838bb5ac9175c109dbd9
Joshua Whitney <38142030+whitnj@users.noreply.github.com>
2018-04-06 15:11:47 -0400

    Update and rename showjson.php to jsonTest.php

--- showjson.php
+++
@@ -1,4 +0,0 @@
-<?php
-	include 'class.JSONHandler.php';
-	jsonHandler::jsonGet("facebook.json","https://graph.facebook.com/v2.12/211202900/posts?access_token=1996372527095397|f5a1466a2292aecaa82231793001da87","America/Los_Angeles");//call class and get the static method.
-?>
