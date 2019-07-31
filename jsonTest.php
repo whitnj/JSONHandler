e9de31640c3136b44cb826d1fbbd00c9feeec790
Joshua Whitney <38142030+whitnj@users.noreply.github.com>
2019-07-30 23:16:58 -0700

    Delete jsonTest.php

--- jsonTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-	include 'class.JSONHandler.php';
-	jsonHandler::jsonGet("facebook.json","https://graph.facebook.com/v2.12/211202900/posts?access_token=tokenNumber","America/Los_Angeles");//call class and get the static method.
-?>
